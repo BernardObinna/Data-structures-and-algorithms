609013488cca5e588f3777c39e2a911cb04f348a
Vayne <bernardobinna2@gmail.com>
2022-05-29 22:41:54 +0100

    converted the anagram solution to typescript

--- problemSolvingTechniques/FrequencyCounter_anagramSolution.ts
+++ problemSolvingTechniques/FrequencyCounter_anagramSolution.ts
@@ -1,8 +1,9 @@
-function validAnagram(str1, str2) {
+// the functions takes in 2 strings and checks if they form anagrams using the frequency counter pattern.
+function validAnagram(str1: string, str2: string): boolean {
   if (str1.length !== str2.length) return false;
 
-  let lookup = {};
-  let isAnagram = true;
+  let lookup: {} = {};
+  let isAnagram: boolean = true;
 
   for (let letter of str1.toLowerCase()) {
     lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
@@ -18,5 +19,7 @@ function validAnagram(str1, str2) {
   }
   return isAnagram;
 }
+
+// time complexity O(n)
 
 console.log(validAnagram("aab", "Aba"));
