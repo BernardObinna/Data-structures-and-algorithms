ae1e7f66855cd16c6c1282c3c10c2ed767b1630a
Vayne <bernardobinna2@gmail.com>
2022-05-29 22:17:59 +0100

    converted multiple pointer to typescript

--- dataStructures/RecursiveBinarySearch.ts
+++ dataStructures/RecursiveBinarySearch.ts
@@ -1,18 +1,21 @@
-let result: number | string = 'Value not in array'
+let result: number | string = "Value not in array";
 
-const recursiveBinarySearch = (list: number[], target: number): number | string => {
-  if (!list.length) return 'The list is empty';
+const recursiveBinarySearch = (
+  list: number[],
+  target: number
+): number | string => {
+  if (!list.length) return "The list is empty";
 
+  let middle: number = Math.floor(list.length / 2);
 
-  let middle: number = Math.floor((list.length) / 2)
-  
-  if(list[middle] === target) return result = "Element found" ;
+  if (list[middle] === target) return (result = "Element found");
+  else if (list[middle] < target)
+    recursiveBinarySearch(list.splice(middle + 1), target);
+  else if (list[middle] > target)
+    recursiveBinarySearch(list.splice(0, middle), target);
+};
 
-  else if (list[middle] < target) recursiveBinarySearch(list.splice(middle + 1), target)
-  else if (list[middle] > target) recursiveBinarySearch(list.splice(0, middle), target)
-  }
-
-recursiveBinarySearch( [1,2,3,4,5,6,7,8,9,10], 10)
-console.log(result)
+recursiveBinarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10);
+console.log(result);
 // time complexity O(log n)
 // space complexity: with tail optimization O(1) but without O(log n)
--- problemSolvingTechniques/MultiplePointer_UniqeValueCounter.ts
+++ problemSolvingTechniques/MultiplePointer_UniqeValueCounter.ts
@@ -1,20 +1,24 @@
-function countUniqueValues(arr){
-    // arr must be a sorted array
-   
-   let first, count = 0;
-   let second = 1;
-   
-   while (second < arr.length){
-       if (arr[first] === arr[second]){
-           second++;
-       } else {
-           first = second;
-           count++;
-           second++;
-       }
-   }
-  
-    return count
+// This takes a sorted array of integers and returns the count of unique integers in the array using the multiple pointer approach
+function countUniqueValues(arr: number[]): number {
+  // arr must be a sorted array
+
+  if (!arr.length) return 0;
+
+  let first: number = 0;
+  let count: number = 1;
+  let second: number = 1;
+
+  while (second < arr.length) {
+    if (arr[first] === arr[second]) {
+      second++;
+    } else {
+      first = second;
+      count++;
+      second++;
+    }
   }
-  
-  console.log(countUniqueValues([1,1,1,1,1,1,2,2,2,3,3,3,4]))
+
+  return count;
+}
+
+console.log(countUniqueValues([1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4]));
